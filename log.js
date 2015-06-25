751731088ec75b53d85039cf3e955901e337ca11
Ubuntu <ubuntu@ip-172-31-30-216.ap-northeast-1.compute.internal>
2015-06-25 09:55:04 +0000

    first commit

---
+++ log.js
@@ -0,0 +1,28 @@
+module.exports = function(mongoose){
+  var LogSchema = new mongoose.Schema({
+  	update_id: {
+  		type: Number,
+  		unique: true
+  	},
+  	message: {
+  		message_id: Number,
+  		from: {
+  			id: Number,
+  			first_name: String,
+  			username: String
+  		},
+  		chat: {
+  			id: Number,
+  			first_name: String,
+  			username: String
+  		},
+  		text: String
+  	},     
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  });
+
+  return mongoose.model('Log', LogSchema);
+};
