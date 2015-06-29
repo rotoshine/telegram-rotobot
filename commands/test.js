4492d6fc0655b6be7f15fed1c04ca0289c2e9589
김태희(로토) <rotoshine@coupang.com>
2015-06-29 19:08:59 +0900

    창꼬형을 위한 기능 추가

--- commands/test.js
+++ commands/test.js
@@ -1,12 +1,22 @@
 var telegram = require('../telegram');
 
+var REPLY_COMMENT = {
+	39598673: 'Oi!!!',
+	59496128: '가슴 만지게 해주세요!'
+};
+
 module.exports = {
 	commandKeywords: ['/test'],
 	description: '봇의 생존여부를 테스트할 수 있습니다',
 	run: function(message, commandParam, callback){
+		var replyComment = 'hello world!';
+
+		if(REPLY_COMMENT.hasOwnProperty(message.from.id)){
+			replyComment = REPLY_COMMENT[message.from.id];
+		}
 		return telegram.sendMessage({
 			chat_id: message.chat.id,
-			text: 'hello world! ' + commandParam,
+			text: replyComment,
 			callback: callback
 		});
 	}
